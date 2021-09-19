08861dcf885979331ccdc87c33124be32d2e84a9
Khaled Romeh <khaledmromeh@gmail.com>
2021-09-19 16:46:53 +0200

    Added maps to addCenter and Center model & changed review model to include is_reported

--- models/review.js
+++ models/review.js
@@ -6,7 +6,8 @@ const reviewSchema = new mongoose.Schema({
     date: Date,
     vaccination_code: Number,
     id_digits: Number,
-    rating: Number
+    rating: Number,
+    is_reported: Boolean
 })
 
 module.exports = mongoose.model("Review", reviewSchema);
